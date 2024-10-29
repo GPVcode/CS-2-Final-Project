6a9ec8d55a616267fba7efd1e587afe24c6ec57e
GPV <gvill005@gmail.com>
2024-10-28 21:12:49 -0700

    clean up code

--- Debts.h
+++ Debts.h
@@ -4,27 +4,29 @@ protected:
     int totalDebt;
     double interestRate;
 public: 
-    Debts(int initialDebt = 0); // contructor
+    Debts(int initialDebt, double initialInterest) : totalDebt(initialDebt), interestRate(initialInterest){}; // constructor
 
-    virtual ~Debts(); // Virtual destructor for proper cleanup in derived classes
+    virtual ~Debts(){}; // Virtual destructor for proper cleanup in derived classes
 
+    // Getter functions
     // Public virtual methods for overriding
-    virtual int getTotalDebt() const;
-    virtual void setTotalDebt(int newDebt);
-    
+    virtual int getTotalDebt() const {
+        return totalDebt;
+    };
     virtual double getInterestRate() const {
         return interestRate;
     }
 
+    // Getter Functions
+    virtual void setTotalDebt(int newDebt) {
+        totalDebt = newDebt;
+    };
     virtual void setInterestRate(double rate) {
         interestRate = rate;
     }
 };
 
-Debts::Debts(int initialDebt) : totalDebt(initialDebt){
-
-}
-
-Debts::~Debts()
-{
-}
+// I need to see how much it costs if I paid x amount a month on my credit card.
+    // debt class - totalDebt amount, interest rate, 
+// How long would it take to pay down using x payment amounts if my deTimbt grows in interest at n percent.
+// I need to see how much I'd save if I put x amount monthly in an account returning at an n percent yield.
